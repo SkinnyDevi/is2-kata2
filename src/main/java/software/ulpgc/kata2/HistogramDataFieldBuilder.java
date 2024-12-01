6eec894e7db042d7cd7fa3289708a7bd644d4da3
SkinnyDevi <skinnydevi.dev@gmail.com>
2024-12-01 12:22:05 +0000

    Kata2 redo

--- src/main/java/software/ulpgc/kata2/HistogramDataFieldBuilder.java
+++
@@ -1,7 +0,0 @@
-package software.ulpgc.kata2;
-
-import java.util.List;
-
-public interface HistogramDataFieldBuilder {
-    List<HistogramDataField> build();
-}
--- src/main/java/software/ulpgc/kata2/TitleStatisticDisplayable.java
+++
@@ -1,5 +0,0 @@
-package software.ulpgc.kata2;
-
-public interface TitleStatisticDisplayable {
-    void display();
-}
