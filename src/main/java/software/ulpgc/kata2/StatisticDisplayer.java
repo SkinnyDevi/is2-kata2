8575838bd7efd57b83d56a26cfa2226ec15fe9ef
SkinnyDevi <skinnydevi.dev@gmail.com>
2024-12-01 12:45:59 +0000

    Kata2 improved

---
+++ src/main/java/software/ulpgc/kata2/StatisticDisplayer.java
@@ -0,0 +1,5 @@
+package software.ulpgc.kata2;
+
+public interface StatisticDisplayer {
+    void display();
+}
---
+++ src/main/java/software/ulpgc/kata2/TitleDeserializer.java
@@ -0,0 +1,5 @@
+package software.ulpgc.kata2;
+
+public interface TitleDeserializer {
+    Title deserializer(String data);
+}
